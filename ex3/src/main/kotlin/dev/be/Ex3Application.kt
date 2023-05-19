30c2d2589e87e47371dcdd84b39f862acab830b8
mrcocoball <gunmaru93@gmail.com>
2023-05-19 23:16:16 +0900

    #0519 - 초기 세팅

---
+++ ex3/src/main/kotlin/dev/be/Ex3Application.kt
@@ -0,0 +1,11 @@
+package dev.be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ex3Application
+
+fun main(args: Array<String>) {
+    runApplication<Ex3Application>(*args)
+}
---
+++ ex3/src/test/kotlin/dev/be/Ex3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ex3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
